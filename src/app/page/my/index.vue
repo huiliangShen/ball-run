c1f3ebc72c9817618f575fa228e38627269371fa
banruo <291233404@qq.com>
2021-01-16 19:06:35 +0800

    commit

--- src/app/page/my/index.vue
+++ src/app/page/my/index.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="my">
         <div :class="['ball', {'active' : flag}]" ref="bal" @click="handleShowLine">
-            <div class="inner"></div>
+<!--            <div class="inner"></div>-->
         </div>
-        <div class="cycle" @click="choose"></div>
         <button @click="choose">点击触发小球动画</button>
-       <!-- <VLine :show="showLine"/>-->
     </div>
 </template>
 
 <script>
-    // import VLine from '@/app/components/line'
     export default {
         data() {
             return {
@@ -18,9 +15,6 @@
                 showLine: false
             }
         },
-        /* components: {
-            VLine
-        }, */
         methods: {
             handleShowLine() {
                 this.showLine = true
@@ -42,77 +36,38 @@
 
 <style lang="less" scoped>
     .my {
-      /*  display: flex;
-        align-items: center;
-        justify-content: center;*/
         height: 100%;
 
-        .cycle {
-            width: 120px;
-            height: 120px;
-            background: #146147;
-            animation-name: cycle;
-            // animation-fill-mode: backwards;
-            animation-duration: 1.5s;
-            // animation-play-state: running;
-            animation-iteration-count: infinite;
-        }
-
         .ball {
+            transition: all .5s linear;
             position: absolute;
             top: 100px;
-            right: 100px;
-            transition: all .5s cubic-bezier(0.46, -0.13, 0.86, 0.00);
+            left: 600px;
             width: 50px;
             height: 50px;
-            background: #fff;
-            /*top: 100px;*/
-            transform: translate3d(0, 100px, 0);
+            background: #000;
+            border-radius: 50%;
+            /*transform: translate3d(0,0,0);*/
 
             &.active {
+                /*top: 1100px;*/
                 top: 1100px;
-                /*transform: translate3d(0, 1100px, 0);*/
-
+                left: 100px;
+                /*transform: translate3d(-300px, 1100px, 0);*/
                 .inner {
                     left: 100px;
-                   /*transform: translate3d(100px, 0, 0);*/
                 }
             }
 
             .inner {
-                /*position: absolute;*/
+                position: absolute;
+                left: 600px;
                 transition: all .5s linear;
                 width: 40px;
                 height: 40px;
                 border-radius: 50%;
                 background: #000;
-                /*left: 600px;*/
-                /*transform: translate3d(600px, 0, 0);*/
             }
-        }
-    }
-
-    @keyframes cycle {
-        0% {
-            border-radius: 0;
-            transform: scale(1) rotate(0deg);
-        }
-        50% {
-            border-radius: 50%;
-            transform: scale(.5) rotate(180deg);
-        }
-        100% {
-            border-radius: 0;
-            transform: scale(1) rotate(360deg);
-        }
-    }
-
-    @keyframes slidein {
-        from {
-            transform: translate3d(0px, 0px, 0)
-        }
-        to {
-            transform: translate3d(200px, 200px, 0)
         }
     }
 </style>
