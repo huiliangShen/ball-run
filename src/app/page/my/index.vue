95ab4a025051f613a56a9ecb6c440ed49fef5bf2
banruo <291233404@qq.com>
2021-01-17 16:09:13 +0800

    create translate type

--- src/app/page/my/index.vue
+++ src/app/page/my/index.vue
@@ -39,30 +39,20 @@
         height: 100%;
 
         .ball {
+            position: fixed;
             transition: all .5s cubic-bezier(0,-0.3, 1, 0.17);
-            position: absolute;
-            top: 100px;
-            /*left: 600px;*/
-           /* width: 50px;
-            height: 50px;
-            background: #fff; */
-            /*border-radius: 50%;*/
-            /*transform: translate3d(0,0,0);*/
+            transform: translate3d(0,100px,0);
 
             &.active {
-                /*top: 1100px;*/
-                top: 1100px;
-                /*left: 100px;*/
-                /*transform: translate3d(-300px, 1100px, 0);*/
+                transform: translate3d(0, 1100px, 0);
                 .inner {
-                    left: 100px;
+                    transform: translate3d(100px, 0, 0);
                 }
             }
 
             .inner {
                 transition: all .5s linear;
-                position: absolute;
-                left: 600px;
+                transform: translate3d(600px, 0, 0);
                 width: 40px;
                 height: 40px;
                 border-radius: 50%;
